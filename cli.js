dc6114fd72603270244cba80217448f5daf94294
Robert Oroszi <robert@oroszi.net>
2019-01-03 19:48:29 +0100

    run on init

--- cli.js
+++ cli.js
@@ -27,24 +27,30 @@ function report (id, data) {
     }
   });
 }
-const job = new CronJob(config.schedule, async function () {
-  const results = {};
-  for (const station of config.stations) {
-    try {
-      console.log('scraping %s', station.url);
-      const data = await parser(station.url);
-      results[station.id] = data;
-      await report(station.id, data);
-    } catch (x) {
-      console.log('error occured while processing %s with id of %s', station.url, station.id);
-      console.error(x);
-      await report(station.id);
+
+console.log('app is starting with schedule of %s to scrape the following stations:\n%s', config.schedule, config.stations.map(s => s.url).join('\n'));
+
+const job = new CronJob({
+  cronTime: config.schedule,
+  runOnInit: true,
+  start: true,
+  onTick: async function () {
+    const results = {};
+    for (const station of config.stations) {
+      try {
+        console.log('scraping %s', station.url);
+        const data = await parser(station.url);
+        results[station.id] = data;
+        await report(station.id, data);
+        console.log('data added to %s', station.id);
+      } catch (x) {
+        console.log('error occured while processing %s with id of %s', station.url, station.id);
+        console.error(x);
+        await report(station.id);
+      }
     }
+
+    console.log(JSON.stringify(results, null, 2));
   }
+});
 
-  console.log(JSON.stringify(results, null, 2));
-}, null, true);
-
-console.log('app has been started with schedule of %s to scrape the following stations:\n%s', config.schedule, config.stations.map(s => s.url).join('\n'));
-
-job.start();
